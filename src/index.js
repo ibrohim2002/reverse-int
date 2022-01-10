8408cd3ca4542d5d6301dcede6c6d0a9cac57adb
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2022-01-10 19:42:08 +0500

    feat: reversing integer number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let abs = Math.abs(n)
+  let x = abs.toString()
+  let length = x.length
+  let reverse = ""
+    for(let i=0; i < length; i++){
+     reverse += x.slice(length-i-1,length-i)
+  }
+    return reverse
 }
